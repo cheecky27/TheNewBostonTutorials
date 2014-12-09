3f6318cf0e6017fc21761c48ad33d723d6595dd6
rosselle27 <rosselle_27@yahoo.com>
2014-12-09 16:52:43 +0800

    The New Boston Tutorial lesson 19 Continuation of creating a Menu using ListActivity

--- src/com/example/practice/Menu.java
+++ src/com/example/practice/Menu.java
@@ -3,6 +3,7 @@ package com.example.practice;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Menu extends ListActivity{
@@ -13,6 +14,7 @@ public class Menu extends ListActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(Menu.this,android.R.layout.simple_list_item_1, classes));
 	}
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
