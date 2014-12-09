a09d46323cab0f048fe1b6f746dd8752be30c0d0
rosselle27 <rosselle_27@yahoo.com>
2014-12-09 14:46:48 +0800

    The New Boston Tutorial Episode 14

--- src/com/example/practice/Splash.java
+++ src/com/example/practice/Splash.java
@@ -4,8 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Splash extends Activity{
-	public void onCreate(Bundle TravisLovebacon){
-		super.onCreate(TravisLovebacon);
+	public void onCreate(Bundle TravisLoveBacon){
+		super.onCreate(TravisLoveBacon);
 		setContentView(R.layout.splash);
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+
+				}catch (InterruptedException e){
+
+				}finally{
+
+				}
+			}
+		};
+		timer.start();
 	}
 }
