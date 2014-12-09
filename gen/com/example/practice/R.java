69c397af4ad265b4ce9ba42b0f183fe1284b3d05
rosselle27 <rosselle_27@yahoo.com>
2014-12-09 18:25:40 +0800

    The New Boston Tutorial Lesson 22 Creating a new Layout

--- gen/com/example/practice/R.java
+++ gen/com/example/practice/R.java
@@ -1199,6 +1199,7 @@ containing a value of this type.
         public static final int activity_main=0x7f030018;
         public static final int splash=0x7f030019;
         public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int txt=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0d0000;
