64e799e1fec0432982d6d27e2892ec1fd5a22a39
rosselle27 <rosselle_27@yahoo.com>
2014-12-10 21:26:01 +0800

    The New Boston Lesson 26 final keyword

--- src/com/example/practice/TextPlay.java
+++ src/com/example/practice/TextPlay.java
@@ -2,6 +2,7 @@ package com.example.practice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,11 +12,20 @@ public class TextPlay extends Activity{
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.txt);
-		
+
 		Button chkCmd = (Button)findViewById(R.id.bResult);
-		ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
+		final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
 		EditText input = (EditText) findViewById(R.id.etCommand);
 		TextView display = (TextView) findViewById(R.id.tvResults);
-		
+
+		passTog.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				if(passTog.isChecked()){
+
+				}else{
+
+				}
+			}
+		});
 	}
 }
