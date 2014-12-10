60b5f513838101f9b7d9c9fe98b22427727f5163
rosselle27 <rosselle_27@yahoo.com>
2014-12-10 21:35:09 +0800

    The New Boston Lesson 27

--- src/com/example/practice/TextPlay.java
+++ src/com/example/practice/TextPlay.java
@@ -2,6 +2,7 @@ package com.example.practice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,15 +16,15 @@ public class TextPlay extends Activity{
 
 		Button chkCmd = (Button)findViewById(R.id.bResult);
 		final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
-		EditText input = (EditText) findViewById(R.id.etCommand);
+		final EditText input = (EditText) findViewById(R.id.etCommand);
 		TextView display = (TextView) findViewById(R.id.tvResults);
 
 		passTog.setOnClickListener(new View.OnClickListener(){
 			public void onClick(View v){
 				if(passTog.isChecked()){
-
+					input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
 				}else{
-
+					input.setInputType(InputType.TYPE_CLASS_TEXT);
 				}
 			}
 		});
