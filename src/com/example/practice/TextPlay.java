8b570f682e28280ca02e75611d9004176d4b33c7
rosselle27 <rosselle_27@yahoo.com>
2014-12-10 21:50:57 +0800

    The New Boston Lesson 29

--- src/com/example/practice/TextPlay.java
+++ src/com/example/practice/TextPlay.java
@@ -3,6 +3,7 @@ package com.example.practice;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +18,7 @@ public class TextPlay extends Activity{
 		Button chkCmd = (Button)findViewById(R.id.bResult);
 		final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
 		final EditText input = (EditText) findViewById(R.id.etCommand);
-		TextView display = (TextView) findViewById(R.id.tvResults);
+		final TextView display = (TextView) findViewById(R.id.tvResults);
 
 		passTog.setOnClickListener(new View.OnClickListener(){
 			public void onClick(View v){
@@ -33,10 +34,12 @@ public class TextPlay extends Activity{
 			public void onClick(View v){
 				String check = input.getText().toString();
 				if(check.contentEquals("left")){
-					
+					display.setGravity(Gravity.LEFT);
 				}else if(check.contentEquals("center")){
-					
+					display.setGravity(Gravity.CENTER);
 				}else if(check.contentEquals("right")){
+					display.setGravity(Gravity.RIGHT);
+				}else if(check.contentEquals("blue")){
 					
 				}
 			}
