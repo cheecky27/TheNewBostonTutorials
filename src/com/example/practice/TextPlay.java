fd4652c51d7a4f7cbb07fe14707c4936aaf49cc9
rosselle27 <rosselle_27@yahoo.com>
2014-12-11 10:00:58 +0800

    The New Boston Android Tutorial Lesson 34

--- src/com/example/practice/TextPlay.java
+++ src/com/example/practice/TextPlay.java
@@ -13,7 +13,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class TextPlay extends Activity{
+public class TextPlay extends Activity implements View.OnClickListener{
 
 	Button chkCmd;
 	ToggleButton passTog;
@@ -25,56 +25,61 @@ public class TextPlay extends Activity{
 		setContentView(R.layout.txt);
 
 		baconAndEggs();
-	
-		passTog.setOnClickListener(new View.OnClickListener(){
-			public void onClick(View v){
-				if(passTog.isChecked()){
-					input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-				}else{
-					input.setInputType(InputType.TYPE_CLASS_TEXT);
-				}
-			}
-		});
 
-		chkCmd.setOnClickListener(new View.OnClickListener(){
-			public void onClick(View v){
-				String check = input.getText().toString();
-				if(check.contentEquals("left")){
-					display.setGravity(Gravity.LEFT);
-				}else if(check.contentEquals("center")){
-					display.setGravity(Gravity.CENTER);
-				}else if(check.contentEquals("right")){
-					display.setGravity(Gravity.RIGHT);
-				}else if(check.contentEquals("blue")){
-					display.setTextColor(Color.BLUE);
-				}else if(check.contains("WTF")){
-					Random crazy = new Random();
-					display.setText("WTF");
-					display.setTextSize(crazy.nextInt(75));
-					display.setTextColor(Color.rgb(crazy.nextInt(265), crazy.nextInt(265), crazy.nextInt(265)));
-					switch(crazy.nextInt(3)){
-					case 0:
-						display.setGravity(Gravity.LEFT);
-						break;
-					case 1:
-						display.setGravity(Gravity.CENTER);
-						break;
-					case 2:
-						display.setGravity(Gravity.RIGHT);
-					}
-				}else{
-					display.setText("invalid");
-					display.setGravity(Gravity.CENTER);
-					display.setTextColor(Color.WHITE);
-				}
-			}
-		});
+		passTog.setOnClickListener(this);
+		chkCmd.setOnClickListener(this);
+
 	}
-	
+
 	public void baconAndEggs(){
-		Button chkCmd = (Button)findViewById(R.id.bResult);
+		chkCmd = (Button)findViewById(R.id.bResult);
 		passTog = (ToggleButton) findViewById(R.id.tbPassword);
 		input = (EditText) findViewById(R.id.etCommand);
 		display = (TextView) findViewById(R.id.tvResults);
+	}
+
+	@Override
+	public void onClick(View view) {
+		// TODO Auto-generated method stub
+		switch(view.getId()){
+		case R.id.bResult:
+			String check = input.getText().toString();
+			if(check.contentEquals("left")){
+				display.setGravity(Gravity.LEFT);
+			}else if(check.contentEquals("center")){
+				display.setGravity(Gravity.CENTER);
+			}else if(check.contentEquals("right")){
+				display.setGravity(Gravity.RIGHT);
+			}else if(check.contentEquals("blue")){
+				display.setTextColor(Color.BLUE);
+			}else if(check.contains("WTF")){
+				Random crazy = new Random();
+				display.setText("WTF");
+				display.setTextSize(crazy.nextInt(75));
+				display.setTextColor(Color.rgb(crazy.nextInt(265), crazy.nextInt(265), crazy.nextInt(265)));
+				switch(crazy.nextInt(3)){
+				case 0:
+					display.setGravity(Gravity.LEFT);
+					break;
+				case 1:
+					display.setGravity(Gravity.CENTER);
+					break;
+				case 2:
+					display.setGravity(Gravity.RIGHT);
+				}
+			}else{
+				display.setText("invalid");
+				display.setGravity(Gravity.CENTER);
+				display.setTextColor(Color.WHITE);
+			}
+			break;
+		case R.id.tbPassword:
+			if(passTog.isChecked()){
+				input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+			}else{
+				input.setInputType(InputType.TYPE_CLASS_TEXT);
+			}
+			break;
+		}
 	}
 }
