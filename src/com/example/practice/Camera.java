41c16d3f52f369f347c81a2bc5e8ad668f7d18e5
rosselle27 <rosselle_27@yahoo.com>
2014-12-14 11:16:26 +0800

    The New Boston Android Tutorial Lesson 41 added the ImageView and set up the Permissions

--- src/com/example/practice/Camera.java
+++ src/com/example/practice/Camera.java
@@ -1,5 +1,7 @@
 package com.example.practice;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -26,18 +28,24 @@ public class Camera extends Activity implements OnClickListener{
 	}
 	
 	private void initialize(){
-		ib = (ImageButton) findViewById(R.id.ibTakePic);
-		b = (Button) findViewById(R.id.bSetWallPaper);
 		iv = (ImageView) findViewById(R.id.ivReturnedPic);
-		
+		ib = (ImageButton) findViewById(R.id.ibTakePic);
+		b = (Button) findViewById(R.id.bSetWallPaper);	
 		b.setOnClickListener(this);
 		ib.setOnClickListener(this);
 	}
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch(v.getId()){
+		
 		case R.id.bSetWallPaper:
+			try{
+				getApplicationContext().setWallpaper(bmp);
+			} catch(IOException e){
+				e.printStackTrace();
+			}
 			break;
 		case R.id.ibTakePic:
 			i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
@@ -53,6 +61,7 @@ public class Camera extends Activity implements OnClickListener{
 		if(resultCode == RESULT_OK){
 			Bundle extras = data.getExtras();
 			bmp = (Bitmap) extras.get("data");
+			iv.setImageBitmap(bmp);
 		}
 	}
 
